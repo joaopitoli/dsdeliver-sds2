04441abfd13f496e8a1d9b3bd7b4230c5e680b12
Joao Pitoli <joao.contats@gmail.com>
2021-01-08 20:23:23 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,30 @@
+import './styles.css'
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+
+                <a href="https://youtube.com.br" target="_new">
+                    <YoutubeIcon />
+                </a>
+
+                <a href="https://linkedin.com.br" target="_new">
+                    <LinkedinIcon />
+                </a>
+
+                <a href="https://instagram.com.br" target="_new">
+                    <InstagramIcon />
+                </a>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
